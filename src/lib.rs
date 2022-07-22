487002221c4c0a5eda0bff1dcb6195b47b0f61f3
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-07-22 20:28:48 +0200

    Add argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
+pub struct Config {
+    pub verbose: bool,
+}
 
+impl Config {
+    pub fn default() -> Config {
+        Config {
+            verbose: false,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use cargo_ws_manage::Config;
+
+use argparse::{ArgumentParser, StoreTrue};
+
 fn main() {
-    println!("Hello, world!");
+    let mut cfg = Config::default();
+    {
+        let mut parser = ArgumentParser::new();
+
+        parser.set_description("Cargo workspace management");
+
+        parser.refer(&mut cfg.verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue, "Verbose output");
+
+        parser.parse_args_or_exit();
+    }
 }
